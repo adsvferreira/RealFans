0b5de9ed167c514c0d4fced77080240076f80d99
1DeadPixel <mc.valente@protonmail.ch>
2023-11-03 20:13:28 +0000

    :sparkles: Add NFT metadata

--- metadata/nft_metadata.py
+++ metadata/nft_metadata.py
@@ -1,6 +1,6 @@
 nft_template = {
     "name": "",
-    "description": "Real Fans NFT gift",
+    "description": "",
     "image": "",
     "attributes": [
         {
@@ -8,3 +8,62 @@ nft_template = {
         }
     ]
 }
+
+nft_bronze = {
+    "name": "Bronze",
+    "description": nft_template["description"],
+    "image": "ipfs://bafybeifgvakzouf5hskbocu7crw3qyflqefblyllmbo3bikp7rwbys4jwu",
+    "attributes": [
+        {
+            "base_value": 0.0001
+        }
+    ]
+}
+
+nft_silver = {
+    "name": "Silver",
+    "description": nft_template["description"],
+    "image": "ipfs://bafybeihfef7gikj32p42ue2oxtmosu3g2pefvd4hzf4k5fesik7ibj5g7m",
+    "attributes": [
+        {
+            "base_value": 0.001
+        }
+    ]
+}
+
+nft_gold = {
+    "name": "Gold",
+    "description": nft_template["description"],
+    "image": "ipfs://bafybeieixddf35zcyfsdmjzfitaxxdsiq3kh23iyasvxxlce2yeb27i7be",
+    "attributes": [
+        {
+            "base_value": 0.01
+        }
+    ]
+}
+
+nft_platinum = {
+    "name": "Platinum",
+    "description": nft_template["description"],
+    "image": "ipfs://bafybeiajozkqdd4yhyjixhxcyfalc42c6fsgcpvkj7tco67poe7m6dzoei",
+    "attributes": [
+        {
+            "base_value": 0.1
+        }
+    ]
+}
+
+nft_diamond = {
+    "name": "Diamond",
+    "description": nft_template["description"],
+    "image": "ipfs://bafybeidclzofxsv7mfkj3jlgzfsgecuvmf6yaxme4347otploxw5mdrrea",
+    "attributes": [
+        {
+            "base_value": 1
+        }
+    ]
+}
+
+
+
+
