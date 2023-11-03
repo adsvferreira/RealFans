1cd974c96cafdbd290239ac8d74a20cf11a8d5a5
dynalz <tonyramos237@hotmail.com>
2023-11-03 17:20:01 +0000

    :bug: fix port

--- realfans_api/main.py
+++ realfans_api/main.py
@@ -9,15 +9,16 @@ from realfans_api.routes.helloworld import router as hello_world_router
 app = FastAPI()
 app.include_router(hello_world_router)
 
+PORT = 8000
 
 def main() -> NoReturn:
     print(
         f"""
 {bcolors.PURPLE}Loading API...{bcolors.END_COLOR}
-Visit {bcolors.YELLOW}http://localhost:9000/docs{bcolors.END_COLOR} for documentation
+Visit {bcolors.YELLOW}http://localhost:{PORT}/docs{bcolors.END_COLOR} for documentation
 """
     )
-    uvicorn.run(app, host="0.0.0.0", port=9000, reload=False, workers=1)
+    uvicorn.run(app, host="0.0.0.0", port=PORT, reload=False, workers=1)
     cleanup_server()
 
 if __name__ == "__main__":
