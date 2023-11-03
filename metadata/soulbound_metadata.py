6a1eebeb0e75abc1a3c75b9aab6b9d6a15d0243f
1DeadPixel <mc.valente@protonmail.ch>
2023-11-03 21:45:53 +0000

    :construction: Work In Progress

---
+++ metadata/soulbound_metadata.py
@@ -0,0 +1,54 @@
+soulbound_template = {
+    "name": "",
+    "description": "",
+    "image": "",
+    "attributes": [
+        {
+          "quote": ""  
+        }
+    ]
+}
+
+soulbound_donate_1 = {
+    "name": "The Gift of Gifting",
+    "description": "Donate one NFT.",
+    "image": "",
+    "attributes": [
+        {
+            "quote": "Your donation game is strong... even if it's a one-hit wonder!"
+        }
+    ]
+}
+
+soulbound_donate_2 = {
+    "name": "The Patron",
+    "description": "Donate five NFTs.",
+    "image": "",
+    "attributes": [
+        {
+            "quote": "Five donations? You're not just a donor; you're a patron of generosity! Your wallet must be feeling lighter, but your heart is shining brighter."
+        }
+    ]
+}
+
+soulbound_donate_3 = {
+    "name": "Philanthropist",
+    "description": "Donate 10 NFTs.",
+    "image": "",
+    "attributes": [
+        {
+            "quote": "Your donations have lit up the path for countless dreams to come true, turning despair into hope and want into abundance. You are a shining beacon of benevolence, a symbol of humanity's boundless potential for good."
+        }
+    ]
+}
+
+soulbound_people_1 = {
+    "name": "Double Trouble",
+    "description": "Donate to 2 different people",
+    "image": "",
+    "attributes": [
+        {
+            "quote": "You've not only opened your heart to one but two fortunate souls, spreading kindness in stereo."
+        }
+    ]
+}
