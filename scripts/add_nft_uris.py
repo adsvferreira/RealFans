32a73c082ae62e4675c256d375345bbadb32cbdd
1DeadPixel <mc.valente@protonmail.ch>
2023-11-04 01:29:56 +0000

    :bug: qf

--- scripts/add_nft_uris.py
+++ scripts/add_nft_uris.py
@@ -1,13 +1,12 @@
 from metadata.nft_uris import NFT_URIS
 from brownie import config, network, accounts, NFTGifts
 
-dev_wallet = accounts.add(config["wallets"]["from_key_1"])
+def add_nft_uris(dev_wallet):
+    NFT_CONTRACT = config["networks"][network.show_active()]["nft_gifts_address"]
 
-NFT_CONTRACT = config["networks"][network.show_active()]["nft_gifts_address"]
+    nft = NFTGifts.at(NFT_CONTRACT)
 
-nft = NFTGifts.at(NFT_CONTRACT)
+    for uri in NFT_URIS.values():
+        nft.addNewGiftURI(uri["uri"], uri["eth_value"], {"from": dev_wallet})
 
-for uri in NFT_URIS.values():
-    nft.addNewGiftURI(uri["uri"], uri["eth_value"])
-
-print("Added all NFT URIs.")
+    print("Added all NFT URIs.")
--- scripts/add_soulbound_uris.py
+++ scripts/add_soulbound_uris.py
@@ -1,13 +1,13 @@
 from metadata.soulbound_uris import SOULBOUND_URIS
 from brownie import config, network, accounts, SoulboundBadges
 
-dev_wallet = accounts.add(config["wallets"]["from_key_1"])
 
-SOULBOUND_CONTRACT = config["networks"][network.show_active()]["soulbound_address"]
+def add_soulbound_uris(dev_wallet):
+    SOULBOUND_CONTRACT = config["networks"][network.show_active()]["soulbound_address"]
 
-soulbound_contract = SoulboundBadges.at(SOULBOUND_CONTRACT)
+    soulbound_contract = SoulboundBadges.at(SOULBOUND_CONTRACT)
 
-for uri in SOULBOUND_URIS.values():
-    soulbound_contract.addNewBadgeURI(uri)
+    for uri in SOULBOUND_URIS.values():
+        soulbound_contract.addNewBadgeURI(uri, {"from": dev_wallet})
 
-print("Added all Soulbound URIs.")
+    print("Added all Soulbound URIs.")
