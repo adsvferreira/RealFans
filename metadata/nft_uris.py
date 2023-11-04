538f5dd0469779d0756fa6abee2ff5432473cc8f
1DeadPixel <mc.valente@protonmail.ch>
2023-11-04 18:11:41 +0000

    :bug: Fix ipfs links

--- metadata/nft_uris.py
+++ metadata/nft_uris.py
@@ -1,22 +1,22 @@
 NFT_URIS = {
     "BRONZE_URI": {
-        "uri": "ipfs://bafybeiem3hww2qkwzei3r62jx2xu25nax6zaaunaqag6uyaiuyd2hwgxba/Bronze.json",
+        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Bronze.json",
         "eth_value": 1,
     },
     "SILVER_URI": {
-        "uri": "ipfs://bafybeiem3hww2qkwzei3r62jx2xu25nax6zaaunaqag6uyaiuyd2hwgxba/Silver.json",
+        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Silver.json",
         "eth_value": 10,
     },
     "GOLD_URI": {
-        "uri": "ipfs://bafybeiem3hww2qkwzei3r62jx2xu25nax6zaaunaqag6uyaiuyd2hwgxba/Gold.json",
+        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Gold.json",
         "eth_value": 100,
     },
     "PLATINUM_URI": {
-        "uri": "ipfs://bafybeiem3hww2qkwzei3r62jx2xu25nax6zaaunaqag6uyaiuyd2hwgxba/Platinum.json",
+        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Platinum.json",
         "eth_value": 1000,
     },
     "DIAMOND_URI": {
-        "uri": "ipfs://bafybeiem3hww2qkwzei3r62jx2xu25nax6zaaunaqag6uyaiuyd2hwgxba/Diamond.json",
+        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Diamond.json",
         "eth_value": 10000,
     },
 }
