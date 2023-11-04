cf2e4e4ab8e1717b2cc226b1b9f78efb34131332
dynalz <tonyramos237@hotmail.com>
2023-11-04 14:53:45 +0000

    :bug: fixes

--- realfans_api/main.py
+++ realfans_api/main.py
@@ -1,11 +1,8 @@
 import uvicorn
-import asyncio
-import concurrent.futures
 
 from typing import NoReturn
 from fastapi import FastAPI
 
-from realfans_api.tasks import write_events
 from realfans_api.utils.colors import bcolors
 from realfans_api.routes.user import router as user_router
 from realfans_api.utils.cleanup_server import cleanup_server
@@ -17,7 +14,7 @@ app.include_router(user_router)
 PORT = 8000
 
 
-async def main(loop) -> NoReturn:
+def main() -> NoReturn:
     print(
         f"""
 {bcolors.PURPLE}Loading API...{bcolors.END_COLOR}
@@ -27,16 +24,6 @@ Visit {bcolors.YELLOW}http://localhost:{PORT}/docs{bcolors.END_COLOR} for docume
     uvicorn.run(app, host="0.0.0.0", port=PORT, reload=False, workers=1)
     cleanup_server()
 
-    # Write the events into the database
-    loop.run_in_executor(None, lambda: write_events.execute())
-    
-
-    
-
 
 if __name__ == "__main__":
-    loop = asyncio.get_event_loop()
-    try:
-        loop.run_until_complete(main(loop))
-    finally:
-        loop.close()
+    main()
--- realfans_api/tasks/write_events.py
+++ realfans_api/tasks/write_events.py
@@ -1,10 +1,12 @@
-from data.models import Event
-from data.database import my_database
-from scripts.build_contracts import build_contracts
+# from data.models import Event
+from realfans_api.data.database import MyDatabase
+
+# from scripts.build_contracts import build_contracts
+
 
 def execute():
     block = None
-    while True: # POG
+    while True:  # POG
         contracts = build_contracts()
 
-        # Fetch User events 
+        # Fetch User events
