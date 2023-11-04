6c7ff8284092826fd39b76643d4086544ea0d394
1DeadPixel <mc.valente@protonmail.ch>
2023-11-04 04:00:40 +0000

    :construction: Work In Progress

--- realfans_api/tasks/write_events.py
+++ realfans_api/tasks/write_events.py
@@ -1,10 +1,18 @@
+from brownie import chain
 from data.models import Event
 from data.database import my_database
 from scripts.build_contracts import build_contracts
 
+
+
 def execute():
-    block = None
+    from_block = None
+    to_block = "latest"
+    
     while True: # POG
+        
         contracts = build_contracts()
 
-        # Fetch User events 
+        # Fetch User events
+        users = contracts["users"]
+
