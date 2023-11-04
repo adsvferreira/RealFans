07210db4b83f6bc1c17ac9bcca04bcfdc8dbe8bb
1DeadPixel <mc.valente@protonmail.ch>
2023-11-04 18:12:41 +0000

    :bug: Fix ipfs links

--- metadata/nft_uris.py
+++ metadata/nft_uris.py
@@ -1,22 +1,22 @@
 NFT_URIS = {
     "BRONZE_URI": {
-        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Bronze.json",
+        "uri": "https://ipfs.io/ipfs/bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Bronze.json",
         "eth_value": 1,
     },
     "SILVER_URI": {
-        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Silver.json",
+        "uri": "https://ipfs.io/ipfs/bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Silver.json",
         "eth_value": 10,
     },
     "GOLD_URI": {
-        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Gold.json",
+        "uri": "https://ipfs.io/ipfs/bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Gold.json",
         "eth_value": 100,
     },
     "PLATINUM_URI": {
-        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Platinum.json",
+        "uri": "https://ipfs.io/ipfs/bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Platinum.json",
         "eth_value": 1000,
     },
     "DIAMOND_URI": {
-        "uri": "ipfs://bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Diamond.json",
+        "uri": "https://ipfs.io/ipfs/bafybeiaqamevc4eba2dou2pneonc3akgi4iznffvbxaevmgffq23uhtcqu/Diamond.json",
         "eth_value": 10000,
     },
 }
--- scripts/generate_soulbound_jsons.py
+++ scripts/generate_soulbound_jsons.py
@@ -8,16 +8,4 @@ def generate_json(nft_dict: dict):
         json.dump(nft_dict, file)
 
 def main():
-    generate_json(soulbound_donate_1)
-    generate_json(soulbound_donate_2)
-    generate_json(soulbound_donate_3)
-    generate_json(soulbound_people_1)
-    generate_json(soulbound_people_2)
-    generate_json(soulbound_people_3)
-    generate_json(soulbound_fan_1)
-    generate_json(soulbound_fan_2)
-    generate_json(soulbound_fan_3)
-    generate_json(soulbound_eth_1)
-    generate_json(soulbound_eth_2)
-    generate_json(soulbound_eth_3)
     generate_json(soulbound_all_nfts)
