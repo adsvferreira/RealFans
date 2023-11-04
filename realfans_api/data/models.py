50f7c88dd091f44de7e9864e3d75194411cd81e2
1DeadPixel <mc.valente@protonmail.ch>
2023-11-04 04:02:15 +0000

    gkjdg

--- realfans_api/data/models.py
+++ realfans_api/data/models.py
@@ -1,17 +1,9 @@
 from typing import Optional
-from enum import Enum, auto
 from pydantic.dataclasses import dataclass
-
-
-class LeaderboardType(str, Enum):
-    DONATERS = auto()
-    QUESTS = auto()
-    CREATORS = auto()
 
 
 @dataclass
 class TwitterProfile:
-    username: str
     name: Optional[str] = None
     avatar: Optional[str] = None
     tweets: Optional[int] = None
@@ -19,29 +11,38 @@ class TwitterProfile:
     following: Optional[int] = None
     created_at: Optional[str] = None
 
+@dataclass
+class Transfer:
+    from_address: str
+    to_address: str
+    value: int
+
+@dataclass
+class Approval:
+    owner: str
+    spender: str
+    value: int
 
 @dataclass
 class Donation:
     sender: str
+    to_address: str
     receiver_twitter_handle: str
+    donator_twitter_handle: str
     gift_uri: str
-    eth_value: float
-    redeemed: bool = False
-
+    eth_value: int
 
 @dataclass
 class Redemption:
     sender: str
     receiver_twitter_handle: str
     gift_token_uri: str
-    eth_value: float
-
+    eth_value: int
 
 @dataclass
 class BadgeMinted:
     to_address: str
     badge_uri: str
-
 
 @dataclass
 class UserAdded:
