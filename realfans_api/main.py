6e91b689085986c6b38e8ba47e72b27b0cc52bda
dynalz <tonyramos237@hotmail.com>
2023-11-05 12:51:43 +0000

    :sparkles: port based

--- realfans_api/main.py
+++ realfans_api/main.py
@@ -1,5 +1,9 @@
+import os
 import uvicorn
 import threading
+from dotenv import load_dotenv, find_dotenv
+
+load_dotenv(find_dotenv())
 
 from typing import NoReturn
 from fastapi import FastAPI
@@ -14,7 +18,15 @@ app = FastAPI()
 app.include_router(user_router)
 app.include_router(stats_router)
 
-PORT = 8000
+if not (network := os.getenv("NETWORK_ID", "")):
+    raise Exception("NETWORK_ID ENV VARIABLE NOT DEFINED")
+
+PORT = {
+    "arbitrum-main": 8000,
+    "polygon-zk-main": 8001,
+    "gnosis-main": 8002,
+    "coredao-main": 8003,
+}.get(network)
 
 
 def main() -> NoReturn:
@@ -29,8 +41,6 @@ Visit {bcolors.YELLOW}http://localhost:{PORT}/docs{bcolors.END_COLOR} for docume
     thread.start()
     uvicorn.run(app, host="0.0.0.0", port=PORT, reload=False, workers=1)
     cleanup_server()
-
-
 
 
 if __name__ == "__main__":
